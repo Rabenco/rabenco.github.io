3e8c92c3af9466a6dec8e14f29411e1b06368da2
Christelle Troussard <chrisrabenco@gmail.com>
2023-12-13 17:56:37 +0100

    modif version

--- source/conf.py
+++ source/conf.py
@@ -22,8 +22,8 @@ copyright = '2023, RABENCO - TROUSSARD Christelle'
 author = 'RABENCO - TROUSSARD Christelle'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
-version = '0.1'
+# release = '0.1'
+# version = '0.1'
 
 # -- General configuration ---------------------------------------------------
 
